c4e66967d1246ce3fcd51bd42a91c7706d299c62
Mayuri9823 <51986554+Mayuri9823@users.noreply.github.com>
2019-07-12 19:16:28 +0530

    same line changes

--- model/Empinfo.py
+++ model/Empinfo.py
@@ -9,4 +9,4 @@ class Employee(db.Model): # this cls is a child of db.model .. sqlalchemy--orm
     salary = db.Column('Emp_Salary', db.Integer)
 
 print("changes from git")
-print("hi")
+print("hello")
